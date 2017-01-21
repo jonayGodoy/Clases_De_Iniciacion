e7c949fc385719a071035b66b08b6ae58178f53e
jonayGodoy <jonaydam2@gmail.com>
2017-01-21 05:19:43 +0100

    generate_a_roman_numeral_for_a_given_decimal_number 3 "iii"

--- src/KataRomanNumerals_Codurance/RomanNumeralGenerator.java
+++ src/KataRomanNumerals_Codurance/RomanNumeralGenerator.java
@@ -3,10 +3,13 @@ package KataRomanNumerals_Codurance;
 public class RomanNumeralGenerator {
     public static String romanFor(int decimal)
     {
+        if(decimal == 3){
+            return "III";
+        }
+
         if(decimal == 2){
             return "II";
         }
-
 
         return "I";
     }
--- test/KataRomanNumerals_Condurance/RomanNumeralGeneratorShould.java
+++ test/KataRomanNumerals_Condurance/RomanNumeralGeneratorShould.java
@@ -13,5 +13,9 @@ public class RomanNumeralGeneratorShould {
     public void generate_a_roman_numeral_for_a_given_decimal_number() throws Exception {
         assertThat(RomanNumeralGenerator.romanFor(1), is("I"));
         assertThat(RomanNumeralGenerator.romanFor(2), is("II"));
+        assertThat(RomanNumeralGenerator.romanFor(3), is("III"));
+
+
+
     }
 }
