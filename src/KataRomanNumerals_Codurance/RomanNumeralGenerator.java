cf1cc02e3e931b790af3e325a31304131016b704
jonayGodoy <jonaydam2@gmail.com>
2017-01-21 05:14:08 +0100

    generate_a_roman_numeral_for_a_given_decimal_number 1 "I"

---
+++ src/KataRomanNumerals_Codurance/RomanNumeralGenerator.java
@@ -0,0 +1,12 @@
+package KataRomanNumerals_Codurance;
+
+public class RomanNumeralGenerator {
+    public static String romanFor(int decimal)
+    {
+
+
+        return "I";
+    }
+
+
+}
---
+++ test/KataRomanNumerals_Condurance/RomanNumeralGeneratorShould.java
@@ -0,0 +1,17 @@
+package KataRomanNumerals_Condurance;
+
+
+import KataRomanNumerals_Codurance.RomanNumeralGenerator;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class RomanNumeralGeneratorShould {
+
+    @Test
+    public void generate_a_roman_numeral_for_a_given_decimal_number() throws Exception {
+        assertThat(RomanNumeralGenerator.romanFor(1), is("I"));
+
+    }
+}
