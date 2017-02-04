c3f1a92dd9a30b11f3e0a2adf5b0d8bba9e15eb3
jonay <casa@gmail.com>
2017-02-04 11:49:22 +0000

    passwordIsCorrect

---
+++ KataPasswordValidate/src/Password.java
@@ -0,0 +1,25 @@
+public class Password {
+
+    public boolean validate(String pass) {
+
+        if (greaterThan6(pass)) {
+            String aux = pass.toUpperCase();
+            for(int i =0 ; i < pass.length(); i++){
+                if(aux.charAt(i) == pass.charAt(i)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean greaterThan6(String pass) {
+        if(pass.length() >6) {
+            return  true;
+        }else{
+            return false;
+        }
+    }
+
+}
---
+++ KataPasswordValidate/test/PassShould.java
@@ -0,0 +1,30 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class PassShould {
+
+    @Test
+    public void passwordIsCorrect() throws Exception {
+        Password pwd = new Password();
+        boolean  isGreater = pwd.validate("xxxxxxAb_1");
+
+        Assert.assertTrue(isGreater);
+    }
+
+    @Test
+    public void passGreaterThan6() throws Exception {
+        Password pwd = new Password();
+        boolean  isGreater = pwd.validate("xxAb_1");
+
+        Assert.assertFalse(isGreater);
+    }
+
+    @Test
+    public void passValidateUpperCase() throws Exception {
+       Password pwd = new Password();
+       boolean  isUpperCase = pwd.validate("aaaaaaaa");
+
+       Assert.assertFalse(isUpperCase);
+    }
+
+}
